24df3390a06567b842f404389d5513117040e02f
liamchampton <liam.hampton@hotmail.co.uk>
2021-04-16 14:00:26 +0100

    write get function for dealership

--- functions/get-dealerships.js
+++ functions/get-dealerships.js
@@ -7,6 +7,20 @@
   * @return The output of this action, which must be a JSON object.
   *
   */
+ const md5 = require('spark-md5');
+
+
  function main(params) {
-	return { message: 'Hello World' };
-}
+   return {
+     entries: params.rows.map((row) => { return { 
+         id: row.doc.id,
+         city: row.doc.city,
+         state: row.doc.state,
+         st: row.doc.st,
+         address: row.doc.address,
+         zip: row.doc.zip,
+         lat: row.doc.lat,
+         long: row.doc.long,
+     }})
+   };
+ }
